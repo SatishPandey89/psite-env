134bdc9116e0b95ec5862425c2b0b59784cdb8af
satish <satishpandey89@gmail.com>
2017-05-29 03:39:24 +0530

    module 1 work inprogress

---
+++ buildScript/testSetup.js
@@ -0,0 +1,7 @@
+
+
+//register babel to transpile before our test run.
+require('babel-register')();
+
+//disable webpack features that mocha doesn't understand.
+require.extensions['.css'] = function() {};
